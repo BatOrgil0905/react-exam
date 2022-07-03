18ebd698ea2a24cd43ed50dd9c40ee676c15076d
BatOrgil <btrgl.btr@gmail.com>
2022-07-03 14:02:15 +0800

    update

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Title = () => {
   return (
-    <h1 className='text-2xl text-white w-fit border-b-4 border-blue-500 cursor-pointer duration-300 hover:border-purple-500'>Around The World Trip</h1>
-  )
-}
+    <h1 className="text-2xl text-white w-fit border-b-4 border-blue-500 cursor-pointer duration-300 hover:border-purple-500">
+      Around The World Trip
+    </h1>
+  );
+};
 
-export default Title
+export default Title;
